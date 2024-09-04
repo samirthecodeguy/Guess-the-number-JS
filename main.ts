9c9988649bc7558f53338817588d95d45a02574d
samirthecodeguy <sammirou2014@gmail.com>
2024-09-04 20:11:15 +0100

    add typescript

---
+++ main.ts
@@ -0,0 +1,51 @@
+// Your JavaScript goes here
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector(".guesses");
+const lastResult: HTMLElement = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField: HTMLInputElement = document.querySelector(".guessField");
+
+let guessCount = 1;
+let resetButton;
+
+function checkGuess() {
+    const userGuess = Number(guessField.value);
+
+    if (guessCount === 1) {
+        guesses.textContent = "Previous Guesses";
+    }
+    guesses.textContent += ` ${userGuess}`;
+
+    if (userGuess === randomNumber) {
+        lastResult.textContent = "Congs";
+        lastResult.style.background = "green";
+        lowOrHi.textContent = "";
+        setGameOver();
+    } else if (guessCount == 10) {
+        lastResult.textContent = "!!GAME OVER!!";
+        lowOrHi.textContent = ""
+        setGameOver();
+    } else {
+        lastResult.textContent = "Wrong!";
+        lastResult.style.backgroundColor = "red";
+        if (userGuess < randomNumber) {
+            lowOrHi.textContent = "Last guess was too low"
+        }
+        else if (userGuess > randomNumber) {
+            lowOrHi.textContent = "Last guess was too high"
+        }
+
+    }
+    guessCount++;
+    guessField.focus();
+    guessField.value = "";
+}
+function setGameOver() {
+}
+guessSubmit.addEventListener("click", checkGuess)
+console.log(checkGuess);
+
+// add last number to lowOrHi
